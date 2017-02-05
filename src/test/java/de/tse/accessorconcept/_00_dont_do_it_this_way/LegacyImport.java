0a0d662e70efd17e837a97977fb258134780a2f2
Tino Steinort <tinosteinort@live.de>
2017-02-05 17:31:34 +0100

    Add legacy Import Example

---
+++ src/test/java/de/tse/accessorconcept/_00_dont_do_it_this_way/LegacyImport.java
@@ -0,0 +1,23 @@
+package de.tse.accessorconcept._00_dont_do_it_this_way;
+
+import de.tse.accessorconcept.ByteArrayHelper;
+import de.tse.accessorconcept.MyObject;
+
+/**
+ * Created by Tino on 05.02.2017.
+ */
+public class LegacyImport {
+
+    public MyObject doImport(final byte[] data) {
+
+        final MyObject obj = new MyObject();
+
+        // Die Zahlen entsprechen der Position und Länge der zu lesenden Daten
+        obj.setFirstname(ByteArrayHelper.readString(data, 0, 10));
+        obj.setLastname(ByteArrayHelper.readString(data, 10, 10));
+        obj.setAge(ByteArrayHelper.readInteger(data, 20, 3));
+        // ...
+
+        return obj;
+    }
+}
